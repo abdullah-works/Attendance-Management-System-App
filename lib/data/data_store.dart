75e2cd2d8e5b375b8e74dc61ebca531c7b314b5f
abdullah-works <abdullah.worke@gmail.com>
2024-11-07 12:59:37 +0500

    added dummy instances of updated attendance model

--- lib/data/data_store.dart
+++ lib/data/data_store.dart
@@ -1,4 +1,6 @@
+import 'package:attendance_management_system_app/models/attendance_model.dart';
 import 'package:attendance_management_system_app/widgets/attendance_radio_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AttendanceValue {
   AttendanceValue(
@@ -30,4 +32,53 @@ List<AttendanceValue> attendanceData = [
   AttendanceValue(
       attendanceStatus: AttendanceOptions.markLeave,
       attendanceDate: DateTime.now()),
+];
+
+List<Attendance> userAttendanceList = [
+  Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+
+  ),
+  Attendance(
+    attendanceStatus: 'Absent',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Absent',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Absent',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Present',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),Attendance(
+    attendanceStatus: 'Absent',
+    attendanceDate: Timestamp.fromDate(DateTime.now()),
+    
+  ),
 ];
