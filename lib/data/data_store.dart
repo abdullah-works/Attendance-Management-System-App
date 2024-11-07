cfeb8a546efe9785e8e402043d578dd824d542a7
abdullah-works <abdullah.worke@gmail.com>
2024-11-07 13:02:18 +0500

    removed instances of expired attendance model

--- lib/data/data_store.dart
+++ lib/data/data_store.dart
@@ -1,38 +1,6 @@
 import 'package:attendance_management_system_app/models/attendance_model.dart';
-import 'package:attendance_management_system_app/widgets/attendance_radio_button.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-class AttendanceValue {
-  AttendanceValue(
-      {required this.attendanceStatus, required this.attendanceDate});
-
-  final AttendanceOptions attendanceStatus;
-  final DateTime attendanceDate;
-}
-
-List<AttendanceValue> attendanceData = [
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markAttendance,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markLeave,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markAttendance,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markAttendance,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markAttendance,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markAttendance,
-      attendanceDate: DateTime.now()),
-  AttendanceValue(
-      attendanceStatus: AttendanceOptions.markLeave,
-      attendanceDate: DateTime.now()),
-];
 
 List<Attendance> userAttendanceList = [
   Attendance(
